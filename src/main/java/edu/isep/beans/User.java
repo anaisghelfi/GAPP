38a2e582fef5d8b5269d9959202c0245c69e1256
vico92 <victorien.gauch@gmail.com>
2015-05-15 12:12:55 +0200

    architecture

---
+++ src/main/java/edu/isep/beans/User.java
@@ -0,0 +1,34 @@
+package edu.isep.beans;
+
+public class User {
+	private int id;
+	private String user;
+	private String password;
+	private int type;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+}
