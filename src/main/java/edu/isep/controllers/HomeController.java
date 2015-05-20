c342b2955fe371a6fcc989459f85ecfbbcb4b9ae
vico92 <victorien.gauch@gmail.com>
2015-05-20 12:28:40 +0200

    connexion LDAP

--- src/main/java/edu/isep/controllers/HomeController.java
+++ src/main/java/edu/isep/controllers/HomeController.java
@@ -1,5 +1,8 @@
 package edu.isep.controllers;
 
+import edu.isep.gapp.LdapAccess;
+import edu.isep.beans.LdapObject;
+
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -20,6 +24,28 @@ public class HomeController {
 	public String home() {
 		logger.info("Page d'accueil");
 		
+		return "home";
+	}
+	@RequestMapping(value = "/connexion", method = RequestMethod.POST)
+	public String connexion(@RequestParam("login") String login,@RequestParam("password") String password,Model model){
+		
+		try {
+			LdapObject newUser = LdapAccess.LDAPget(login,password);
+
+			switch (newUser.getType()) {
+			
+				case "eleve":
+					System.out.println("eleve");
+					return "accueilEleve";
+				case "professeur":
+					System.out.println("prof");
+					break;
+			}
+			
+		} catch(Exception e) {
+			System.out.println("utilisateur non trouvé !");
+			return "home";
+		}
 		return "home";
 	}
 	
