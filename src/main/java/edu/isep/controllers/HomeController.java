a3cb1417612c5a92d9567729c9fe3c2ecdae144b
dkoch <dkoch@isep.fr>
2015-06-05 15:18:51 +0200

    Deadline terminé et Recherche Eleve et groupe

--- src/main/java/edu/isep/controllers/HomeController.java
+++ src/main/java/edu/isep/controllers/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	public HomeController(){
-		ApplicationContext context = new ClassPathXmlApplicationContext("file:/Users/brandonemartins/Documents/A2/Web_Techno/GAPP/src/main/java/edu/isep/gapp/Bean.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("file:/Users/David/git/GAPP/src/main/java/edu/isep/gapp/Bean.xml");
 		
 	}
 	
---
+++ src/main/java/edu/isep/daoImp/RechercherJDBCTemplate.java
@@ -0,0 +1,77 @@
+package edu.isep.daoImp;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import edu.isep.beans.Deadline;
+import edu.isep.beans.Eleve;
+import edu.isep.beans.Groupe;
+
+public class RechercherJDBCTemplate {
+
+	private DataSource datasource;
+	private JdbcTemplate jdbcTemplateObject;
+	
+	public void setDataSource(DataSource dataSource){
+		this.datasource = dataSource;
+		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+	}
+	
+			
+public <List>Eleve getNom(String nom){
+		
+		class GroupeMapper implements RowMapper<Eleve> {
+			   public Eleve mapRow(ResultSet rs, int rowNum) throws SQLException{
+				   
+				  Eleve allEleve =  new Eleve();
+				  allEleve.setNom(rs.getString("nom"));
+				  allEleve.setId(rs.getInt("id"));
+				  allEleve.setNom(rs.getString("prenom"));
+				  allEleve.setNom(rs.getString("promo"));
+				  allEleve.setNom(rs.getString("mail"));
+				  allEleve.setNom(rs.getString("groupe"));
+				  allEleve.setNom(rs.getString("note"));
+			      return allEleve;
+			}  
+		}
+		
+		String sql = "SELECT * FROM eleves where nom = ?";
+		Eleve allEleve = jdbcTemplateObject.queryForObject(sql, new Object[]{nom}, new GroupeMapper());
+		return allEleve;
+	}
+	
+	
+	
+//		Récupérer tous les groupes
+		/*public List<Eleve> allEleves(){
+			String sql = "SELECT * FROM eleves WHERE nom = ? ";
+			
+			ArrayList<Eleve> ListEleves =  new ArrayList<Eleve>();
+			
+			List<Map<String,Object>> rows = jdbcTemplateObject.queryForList(sql);
+			
+			for (Map row : rows) {
+				Eleve eleves =  new Eleve();
+				
+				eleves.setId(Integer.parseInt(String.valueOf(row.get("id"))));
+				eleves.setNom((String)row.get("nom"));
+				eleves.setPrenom((String)row.get("prenom"));
+				eleves.setPromo((String) row.get("promo"));
+				eleves.setMail((String) row.get("mail"));		
+				eleves.setGroupe((String) row.get("groupe"));
+				ListEleves.add(eleves);
+			}
+
+			return ListEleves;
+		}*/
+	
+}
