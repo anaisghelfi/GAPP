3a7cbcce314c271fd852ef2c9cf18dc108225eec
vico92 <victorien.gauch@gmail.com>
2015-06-24 00:33:27 +0200

    tri du menu

--- src/main/java/edu/isep/controllers/ExportNoteController.java
+++ src/main/java/edu/isep/controllers/ExportNoteController.java
@@ -1,29 +1,43 @@
 package edu.isep.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 import edu.isep.beans.Eleve;
 import edu.isep.daoImp.ExportNoteJDBCTemplate;
+import edu.isep.daoImp.MainJDBCTemplate;
 
 @Controller
 public class ExportNoteController {
 	
 	private ExportNoteJDBCTemplate daoExportNote;
+	private MainJDBCTemplate daoMain;
+	
 	
 		public ExportNoteController(){
 			
 			ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
 			daoExportNote = (ExportNoteJDBCTemplate) context.getBean("exportNoteDAO");
+			daoMain = (MainJDBCTemplate) context.getBean("mainDAO");
 			
 		}
 		
 		@RequestMapping(value="/exportNote",method = RequestMethod.GET)
-		public String afficher(Model model){
-					
+		public String afficher(HttpSession session,Model model){
+			
+			if(session.getAttribute("type") == "professeur"){
+				int tuteurType = daoMain.tuteurType((String) session.getAttribute("email"));
+				System.out.println(tuteurType);
+				model.addAttribute("typeTuteur",tuteurType);
+			}
+			
+			
 			return "exportNote";
 		}
 		@RequestMapping(value="/exportNote",method = RequestMethod.POST)
